bab9a3cc827b5ca8fc0a0ced22a4162545674df6
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2026-01-05 18:43:11 +0900

    [feature] delete annotation

--- src/main/java/com/oman/domain/youtube/dto/response/VideoSearchResponse.java
+++ src/main/java/com/oman/domain/youtube/dto/response/VideoSearchResponse.java
@@ -14,36 +14,34 @@ import java.util.Optional;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class YoutubeVideoSearchResponse {
+public class VideoSearchResponse {
     private String apiVideoId;
     private String title;
     private String description;
-    private String publishedAt; // ISO 8601 형식의 문자열
+    private String publishedAt;
     private String channelId;
     private String channelTitle;
-    private String defaultThumbnailUrl; // 기본 썸네일 URL
+    private String defaultThumbnailUrl;
 
-    // API 응답 데이터를 DTO로 변환하는 팩토리 메서드
-    public static YoutubeVideoSearchResponse from(SearchResult searchResult) {
+    public static VideoSearchResponse from(SearchResult searchResult) {
         String videoId = searchResult.getId().getVideoId();
         String title = Optional.ofNullable(searchResult.getSnippet().getTitle()).orElse("제목 없음");
         String description = Optional.ofNullable(searchResult.getSnippet().getDescription()).orElse("");
         String publishedAt = Optional.ofNullable(searchResult.getSnippet().getPublishedAt())
-            .map(com.google.api.client.util.DateTime::toStringRfc3339) // DateTime -> ISO 8601 String
+            .map(com.google.api.client.util.DateTime::toStringRfc3339)
             .orElse(null);
         String channelId = searchResult.getSnippet().getChannelId();
         String channelTitle = searchResult.getSnippet().getChannelTitle();
 
-        // 썸네일 정보 가져오기 (고해상도 썸네일 우선)
         String defaultThumbnailUrl = Optional.ofNullable(searchResult.getSnippet().getThumbnails())
-            .map(ThumbnailDetails::getHigh) // high 썸네일 시도
+            .map(ThumbnailDetails::getHigh)
             .map(Thumbnail::getUrl)
             .orElseGet(() -> Optional.ofNullable(searchResult.getSnippet().getThumbnails())
-                .map(ThumbnailDetails::getDefault) // default 썸네일 시도
+                .map(ThumbnailDetails::getDefault)
                 .map(Thumbnail::getUrl)
-                .orElse(null)); // 둘 다 없으면 null
+                .orElse(null));
 
-        return new YoutubeVideoSearchResponse(
+        return new VideoSearchResponse(
             videoId,
             title,
             description,
