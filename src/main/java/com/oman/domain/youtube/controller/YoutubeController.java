570a835506b09c6a45d46bd44f9ecaf4964437a9
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2026-01-05 18:36:26 +0900

    [feature] change return info in YoutubeController

--- src/main/java/com/oman/domain/youtube/controller/YoutubeController.java
+++ src/main/java/com/oman/domain/youtube/controller/YoutubeController.java
@@ -1,6 +1,7 @@
 package com.oman.domain.youtube.controller;
 
-import com.oman.domain.youtube.dto.response.YoutubeVideoSearchResponse;
+import com.oman.domain.youtube.dto.response.VideoSearchResponse;
+import com.oman.domain.youtube.dto.response.VideoWithCommentResponse;
 import com.oman.domain.youtube.service.YoutubeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +19,13 @@ public class YoutubeController {
     private final YoutubeService youtubeService;
 
     /**
-     * 검색어를 기반으로 상위 10개의 유튜브 동영상 정보를 조회합니다.
+     * 검색어를 기반으로 상위 10개의 유튜브 동영상 정보와 첫 댓글 조회
      * @param query 검색어
      * @return 상위 10개 동영상의 정보 리스트
      */
     @GetMapping("/search/videos")
-    public ResponseEntity<List<YoutubeVideoSearchResponse>> searchYoutubeVideos(@RequestParam String query) {
-        List<YoutubeVideoSearchResponse> videoResponses = youtubeService.searchVideosAndRespond(query);
+    public ResponseEntity<List<VideoWithCommentResponse>> searchYoutubeVideos(@RequestParam String query) {
+        List<VideoWithCommentResponse> videoResponses = youtubeService.searchVideosInfo(query);
         return ResponseEntity.ok(videoResponses);
     }
 }
