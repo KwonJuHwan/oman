9503333c1e70f7308072cdf1298c0eba15c73fa6
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2026-01-22 02:23:55 +0900

    [feature] YoutubeIngredient Ingredient Entity

---
+++ src/main/java/com/oman/domain/youtube/entity/YoutubeIngredient.java
@@ -0,0 +1,45 @@
+package com.oman.domain.youtube.entity;
+
+import com.oman.domain.culinary.entity.Ingredient;
+import com.oman.global.common.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import tools.jackson.databind.annotation.EnumNaming;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class YoutubeIngredient extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String extractedName; // NER로 추출된 실제 재료명
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ingredient_id")
+    private Ingredient ingredient;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "youtube_video_id", nullable = false)
+    private YoutubeVideo youtubeVideo;
+
+
+}
