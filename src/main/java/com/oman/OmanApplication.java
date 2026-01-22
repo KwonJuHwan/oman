218a0653d2790faa169df915e7254eff96538623
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2026-01-22 20:10:47 +0900

    [refactor] add setting dotenv

--- src/main/java/com/oman/OmanApplication.java
+++ src/main/java/com/oman/OmanApplication.java
@@ -1,12 +1,17 @@
 package com.oman;
 
 import org.springframework.boot.SpringApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OmanApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
         SpringApplication.run(OmanApplication.class, args);
     }
 
