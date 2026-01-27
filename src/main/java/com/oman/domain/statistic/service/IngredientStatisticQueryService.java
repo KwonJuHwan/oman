b85b5ac6fee86635d7fafa35abaf9b79c679400f
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2026-01-27 17:47:22 +0900

    [feat] implement getIngredientStatistics method

--- src/main/java/com/oman/domain/statistic/service/IngredientStatisticQueryService.java
+++ src/main/java/com/oman/domain/statistic/service/IngredientStatisticQueryService.java
@@ -2,8 +2,13 @@ package com.oman.domain.statistic.service;
 
 import com.oman.domain.culinary.entity.Ingredient;
 import com.oman.domain.culinary.repository.IngredientRepository;
+import com.oman.domain.culinary.service.CulinaryQueryService;
+import com.oman.domain.culinary.service.IngredientQueryService;
+import com.oman.domain.statistic.dto.CulinaryIngredientResponse;
 import com.oman.domain.statistic.dto.IngredientCountDto;
+import com.oman.domain.statistic.repository.IngredientStatisticRepository;
 import com.oman.domain.youtube.repository.YoutubeIngredientRepository;
+import com.oman.domain.youtube.service.YoutubeQueryService;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -15,19 +20,33 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class IngredientStatisticQueryService {
 
-    private final YoutubeIngredientRepository youtubeIngredientRepository;
-    private final IngredientRepository ingredientRepository;
+    private final YoutubeQueryService youtubeQueryService;
+    private final IngredientQueryService ingredientQueryService;
+    private final IngredientStatisticRepository ingredientStatisticRepository;
+    private final CulinaryQueryService culinaryQueryService;
+
 
     public List<IngredientCountDto> getIngredientCountsForCulinary(Long culinaryId) {
-        return youtubeIngredientRepository.countIngredientsByCulinaryId(culinaryId);
+        return youtubeQueryService.getIngredientCountsForCulinary(culinaryId);
     }
 
     public long getTotalVideoCount(Long culinaryId) {
-        return youtubeIngredientRepository.countTotalVideosByCulinaryId(culinaryId);
+        return youtubeQueryService.getTotalVideoCount(culinaryId);
     }
 
     public Optional<Ingredient> findIngredientById(Long id) {
-        return ingredientRepository.findById(id);
+        return ingredientQueryService.findIngredientById(id);
+    }
+
+    public List<CulinaryIngredientResponse> getIngredientStatistics(String culinaryName) {
+        // 검색한 요리가 없을때 처리 필요
+        if (!culinaryQueryService.culinaryExistByName(culinaryName)) {
+            throw new IllegalArgumentException("해당 요리를 찾을 수 없습니다: " + culinaryName);
+        }
+
+        return ingredientStatisticRepository.findAllByCulinaryName(culinaryName).stream()
+            .map(CulinaryIngredientResponse::from)
+            .toList();
     }
 
 }
