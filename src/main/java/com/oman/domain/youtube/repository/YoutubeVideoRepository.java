adf7aff090aa15e63d2e2d82806ed3e3c08af271
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2026-01-28 22:31:44 +0900

    [refactor] add findAllByCulinaryName for test (simply Query)

--- src/main/java/com/oman/domain/youtube/repository/YoutubeVideoRepository.java
+++ src/main/java/com/oman/domain/youtube/repository/YoutubeVideoRepository.java
@@ -5,8 +5,17 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface YoutubeVideoRepository extends JpaRepository<YoutubeVideo, Long> {
 
     List<YoutubeVideo> findAllByApiVideoIdIn(List<String> videoIds);
+
+    @Query("SELECT v FROM YoutubeVideo v " +
+        "JOIN FETCH v.culinary c " +
+        "JOIN FETCH v.videoIngredients vi " +
+        "JOIN FETCH vi.ingredient i " + // 마스터 재료까지 가져오기
+        "WHERE c.name = :name")
+    List<YoutubeVideo> findAllByCulinaryName(@Param("name") String name);
 }
