493e661a8c57fde001adee376057f24212f5b125
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2026-03-03 17:30:27 +0900

    [refactor] replace dto due to API response

--- src/main/java/com/oman/domain/recipe/dto/response/CulinaryRecommendationDto.java
+++ src/main/java/com/oman/domain/recipe/dto/response/CulinaryRecommendationDto.java
@@ -1,10 +1,13 @@
 package com.oman.domain.recipe.dto.response;
 
 import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
 
-public record CulinaryRecommendationDto(
-    String culinaryName,
-    List<RecipeDetail> exactRecipes,    // 정확히 일치하는 영상들
-    List<RecipeDetail> surplusRecipes,  // 재료가 남는 영상들
-    List<RecipeDetail> missingRecipes   // 재료가 더 필요한 영상들
-) {}
+@Getter
+@Builder
+public class CulinaryRecommendationDto {
+    private String name;
+    private String status;
+    private List<IngredientSimpleDto> ingredients;
+}
