c9c3090c060e08f4ce259b68d500c93a35d57904
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2026-03-03 17:30:41 +0900

    [refactor] add ID field

--- src/main/java/com/oman/domain/statistic/dto/CulinaryIngredientResponse.java
+++ src/main/java/com/oman/domain/statistic/dto/CulinaryIngredientResponse.java
@@ -3,6 +3,7 @@ package com.oman.domain.statistic.dto;
 import com.oman.domain.statistic.entity.IngredientStatistic;
 
 public record CulinaryIngredientResponse(
+    Long ingredientId,
     String ingredientName,
     int totalVideoCount,
     int ingredientIncludedVideoCount,
@@ -11,6 +12,7 @@ public record CulinaryIngredientResponse(
     // 엔티티를 DTO로 변환하는 정적 팩토리 메서드
     public static CulinaryIngredientResponse from(IngredientStatistic statistic) {
         return new CulinaryIngredientResponse(
+            statistic.getId(),
             statistic.getIngredient().getName(),
             statistic.getTotalVideoCount(),
             statistic.getIngredientIncludedVideoCount(),
