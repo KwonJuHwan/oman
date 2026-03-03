bfcc6c3366373dde711f045e55a9797b07dbfe18
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2026-03-03 17:36:50 +0900

    [feat] implement AuthResponse for token info serving

---
+++ src/main/java/com/oman/domain/auth/dto/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.oman.domain.auth.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class AuthResponse {
+    private String accessToken;
+    private String refreshToken;
+    private Long memberId;
+}
